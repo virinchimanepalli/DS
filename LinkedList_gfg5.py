ba7372b9d5e179debc4e5d97ea8a0d3fd58caf20
virinchimanepalli <virinchi.msk83@gmail.com>
2020-02-19 17:19:54 +0530

    Removing Duplicates from unsorted linkedlist

---
+++ LinkedList_gfg5.py
@@ -0,0 +1,75 @@
+class Node:          #unsorted linkedlist
+    def __init__(self,data):
+        self.data =data
+        self.next =None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+    def isListEmpty(self):
+        if self.head is None:
+            print("list is empty")
+            return True
+        else:
+            return False
+
+
+    def insert(self,newNode):
+        if self.head is None:
+            self.head = newNode
+
+        else:
+            lastNode = self.head
+            while True:
+                if lastNode.next is None :
+                    break
+                else:
+                    lastNode = lastNode.next
+            lastNode.next = newNode
+
+    def printList(self):
+        if self.head is None:
+            print("list is empty")
+            return
+       
+        currentNode = self.head
+        while True:
+            if currentNode is None: #if it reaches end it will break
+                break
+            print(currentNode.data)#present value
+            currentNode = currentNode.next #increment
+
+    def unsortedduplicate(self):
+        start = self.head
+        while start.next is not None: #loop running till end
+            temp = start
+            while temp.next is not None:
+                if start.data ==temp.next.data:  #1.start = 1;2.start = 2; where numbers are positions of pointer
+                    new = temp.next.next
+                    temp.next =None
+                    temp.next = new
+                else:
+                    temp = temp.next
+            start = start.next
+
+
+linkedList = LinkedList()
+firstNode =Node(1)
+linkedList.insert(firstNode)
+secondNode =Node(2)
+linkedList.insert(secondNode)
+thirdNode =Node(3)
+linkedList.insert(thirdNode)
+fourthNode = Node(45)
+linkedList.insert(fourthNode)
+fifthnode = Node(8)
+linkedList.insert(fifthnode)
+
+
+
+
+linkedList.unsortedduplicate()
+linkedList.printList()
