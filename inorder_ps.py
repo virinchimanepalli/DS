16002d13a00ea13afdbd29a85217f20f9d5bc4da
virinchimanepalli <virinchi.msk83@gmail.com>
2020-04-23 15:16:39 +0530

    Inorder predecessor and succesor for a given key in BST

--- inorder_ps.py
+++ inorder_ps.py
@@ -10,6 +10,9 @@ def insert(root,node):
         root = node
     else:
         if root.val < node.val:
+
+
+
             if root.right is None:
                root.right = node
             else:
@@ -29,38 +32,50 @@ def inorder(root):
         # print(root)
 
 def right_l(root):
-    current = Node
+    # current = Node
+    # current = None
     while root.left is not None:
-        current = current.left
-    return current
+        # current = root
+        root = root.left
+    return root
+
+def  left_r(root):
+    while root.right is not None:
+        root=root.right
+    return root
 
 
 
 
 def find(root,key):
+    current = root
     while True:
         # if root is None:
         #     return key
         
-        if key < root.val:
-            root = root.left
+        if key < current.val:
+            current = current.left
 
-        elif key > root.val:
-            root = root.right
+        elif key > current.val:
+            current = current.right
         else: 
-            print(root.val)
+            # print(current.val)
             break
-    # temp1 = right_l(root.right)
+        
+    temp1 = right_l(current.right)
+    temp2 =left_r(current.left)
+    print(temp1.val)
+    print(temp2.val)
     # print(temp1.val)
     # current = Node
-    ex = root.right
-    # temp = root.right
-    while temp.left is not None:
-        ex = ex.left
-    print(ex.val)
+    # # ex = root.right
+    # # temp = root.right
+    # while temp.left is not None:
+    #     ex = ex.left
+    # print(ex.val)
 
     
-            
+current = Node           
 
 r = Node(50) 
 insert(r,Node(30)) 
