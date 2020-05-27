aba6b8c2ad33893096d50fddc5d3e97e20374559
virinchimanepalli <virinchi.msk83@gmail.com>
2020-05-27 11:46:09 +0530

    display_of_leafnode

--- sum_of_lf.py
+++ sum_of_lf.py
@@ -21,17 +21,10 @@ def insert(root,node):
                 insert(root.left,node)
         
 def sum(rootl,count):   
-    print(count) 
     if rootl.left is None and rootl.right is None:
         count = count +rootl.val
-        # empty =count
-        # print(count)
-        return count.val
-    # elif rootl.left or rootl.right is not None:
-    #     if rootl.left is None:
-    #         rootl.left = rootl.right
-    #     elif rootl.right is None:
-    #         rootl = rootl.left
+        print(count)
+        
     else:
         current = rootl
         if current.left is None:
@@ -43,15 +36,8 @@ def sum(rootl,count):
             currentr = current.right
             sum(currentl,count)
             sum(currentr,count)
-    # print(count)
+    
             
-
-
-        
-        
-
-
-
 def inorder(root):
     if root:
         inorder(root.left)
@@ -67,5 +53,5 @@ insert(r,Node(60))
 insert(r,Node(80)) 
 
 # inorder(r)
-k=(r,0)
-print(k)
+sum(r,0)
+# print(k)
